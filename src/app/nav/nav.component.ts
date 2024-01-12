5b1afaf30943bcb53d7b69a58935d86b8ba1ca26
Arnold <kamstan6@gmail.com>
2024-01-12 16:01:20 -0500

    Added Author summary

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -11,6 +11,10 @@ import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 export class NavComponent {
   navItems = {
     logo_name : "CREATIVE CV",
-    menu_items: ["ABOUT", "SKILLS", "PORTFOLIO", "EXPERINCE", "CONTACT"]
+    menu_items: ["ABOUT", "SKILLS", "PORTFOLIO", "EXPERINCE", "CONTACT"],
+  }
+  banner_content = {
+    author_name: "Arnold Kamanzi",
+    summary : "A passionate full stack developer skilled in both backend, frontend and API development."
   }
 }
